f3d052045bc5a8575a0bb417577778bf87007bb2
somteacodes <stomteacodes@gmail.com>
2022-01-05 19:01:07 +0100

    added recoil library

--- src/components/Cell/CellUtil.ts
+++
@@ -1,8 +0,0 @@
-export const onClickOutsideInputHandler =(event: MouseEvent)=>{
-    if ((event.target as HTMLElement)?.dataset?.cellKey !== "2") {
-        return false
-        // console.log(event.target);
-      } else {
-        console.log("do nothing");
-      }
-}
