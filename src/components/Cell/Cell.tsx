1923309ba1213fd7ef67ec90b4719fb4852b74d4
somteacodes <stomteacodes@gmail.com>
2022-01-05 19:40:14 +0100

    added cell state

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,9 +1,20 @@
-import { FC, useState, useEffect, useRef } from "react";
+import { FC, useState, useEffect, useRef, ChangeEvent } from "react";
 import './Cell.css'
+import {atom, useRecoilState} from 'recoil'
 type CellProps = {};
+const cellValueState = atom({
+  key:'cell',
+  default:"Hello"
+})
+
+
 const Cell: FC<CellProps> = (props) => {
+
+  // states
   const [editMode, setEditMode] = useState(false);
   const inputRef = useRef(null);
+  const [cellValue, setCellValue] = useRecoilState(cellValueState)
+  // functions
   const changeLabelToInput = () => {
     setEditMode(true);
   };
@@ -13,24 +24,30 @@ const Cell: FC<CellProps> = (props) => {
   const onClickOutsideInputHandler = (event: MouseEvent) => {
     if ((event.target as HTMLElement)?.dataset?.cellKey !== "2") {
       changeInputToLabel();
-      console.log(event.target);
+      
     } else {
       console.log("do nothing");
-    }
-
-    // return
+    } 
   };
+
+  const updateCellValue =(event:ChangeEvent<HTMLInputElement>) => {
+    setCellValue(event.target.value)
+  }
+
+  // lifecycles and sideEffects
   useEffect(() => {
     document.addEventListener("click", onClickOutsideInputHandler);
     return document.addEventListener("click", onClickOutsideInputHandler);
   }, []);
+
+  // template
   return (
     <>
       {editMode ? (
-        <input ref={inputRef} data-cell-key="2" className="cellInput" />
+        <input ref={inputRef} value={cellValue} data-cell-key="2" className="cellInput" onChange={updateCellValue}/>
       ) : (
         <div data-cell-key="2" onClick={changeLabelToInput}>
-          {props.children}
+          {cellValue}
         </div>
       )}
     </>
