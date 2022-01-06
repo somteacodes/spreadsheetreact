9c768aca4e4a7a2d0b47ddcd98c0ba3e12c47353
somteacodes <stomteacodes@gmail.com>
2022-01-06 01:17:07 +0100

    implemented Enter key press to defocus cell

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,19 +1,20 @@
-import { FC, useState, useEffect, useRef, ChangeEvent } from "react";
-import './Cell.css'
-import  {useRecoilState} from 'recoil'
+import { FC, useState, useEffect, useRef, ChangeEvent, KeyboardEvent } from "react";
+import "./Cell.css";
+import { useRecoilState } from "recoil";
 
-import {cellValueState} from '../../store/cellStore'
+import { cellValueState } from "../../store/cellStore";
 
 type CellProps = {
-  cellKey:string
+  cellKey: string;
 };
 
 const Cell: FC<CellProps> = (props) => {
-
   // states
   const [editMode, setEditMode] = useState(false);
   const inputRef = useRef(null);
-  const [cellValue, setCellValue] = useRecoilState<string>(cellValueState(props.cellKey))
+  const [cellValue, setCellValue] = useRecoilState<string>(
+    cellValueState(props.cellKey)
+  );
   // functions
   const changeLabelToInput = () => {
     setEditMode(true);
@@ -21,18 +22,21 @@ const Cell: FC<CellProps> = (props) => {
   const changeInputToLabel = () => {
     setEditMode(false);
   };
+  
   const onClickOutsideInputHandler = (event: MouseEvent) => {
     if ((event.target as HTMLElement)?.dataset?.cellKey !== props.cellKey) {
       changeInputToLabel();
-      
-    } else {
-      console.log("do nothing");
     } 
   };
 
-  const updateCellValue =(event:ChangeEvent<HTMLInputElement>) => {
-    setCellValue(event.target.value)
+  const removeInputFocus= (event:KeyboardEvent<HTMLInputElement>)=>{
+      if(event.key ==='Enter'){
+        setEditMode(false);
+      }
   }
+  const updateCellValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setCellValue(event.target.value);
+  };
 
   // lifecycles and sideEffects
   useEffect(() => {
@@ -44,9 +48,20 @@ const Cell: FC<CellProps> = (props) => {
   return (
     <>
       {editMode ? (
-        <input ref={inputRef} value={cellValue} data-cell-key={props.cellKey} className="cellInput" onChange={updateCellValue}/>
+        <input
+          ref={inputRef}
+          value={cellValue}
+          data-cell-key={props.cellKey}
+          className="cellInput"
+          onChange={updateCellValue}
+          onKeyDown={removeInputFocus}
+        />
       ) : (
-        <div data-cell-key={props.cellKey} onClick={changeLabelToInput} className="cellLabel">
+        <div
+          data-cell-key={props.cellKey}
+          onClick={changeLabelToInput}
+          className="cellLabel"
+        >
           {cellValue}
         </div>
       )}
